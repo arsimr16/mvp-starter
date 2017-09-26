7c34ef0b75d4e4904f0b7f697c09efd1d01f17fe
arsimr16 <arsimr16@g.holycross.edu>
2017-09-25 21:23:31 -0700

    create topicSchema and models for each topic; topic data contained in arrays; insert data from arrays into database; create selectRandom functions for each topic

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,6 +1,6 @@
-// set up mongoose connection with mongo database
+// set up mongoose connection with existing mongo database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/topics');
+mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 
@@ -13,48 +13,18 @@ db.once('open', function() {
 });
 
 
-
-
-
-// Set up Schema and Model for each topic
-
-// conversation starters
-var convstartsSchema = mongoose.Schema({
+// Set up schema for all models
+var topicSchema = mongoose.Schema({
   id: {type: Number, unique: true},
   description: String
 });
 
-var Convstarts = mongoose.model('Convstarts', convstartsSchema);
+// Create model (table) for each topic
+var Convstarts = mongoose.model('Convstarts', topicSchema);
+var Jokes = mongoose.model('Jokes', topicSchema);
+var Quotes = mongoose.model('Quotes', topicSchema);
+var Pickups = mongoose.model('Pickups', topicSchema);
 
-// jokes
-var jokesSchema = mongoose.Schema({
-  id: {type: Number, unique: true},
-  description: String
-});
-
-var Jokes = mongoose.model('Jokes', jokesSchema);
-
-// quotes
-var quotesSchema = mongoose.Schema({
-  id: {type: Number, unique: true},
-  description: String
-});
-
-var Quotes = mongoose.model('Quotes', quotesSchema);
-
-// pickup lines
-var pickupsSchema = mongoose.Schema({
-  id: {type: Number, unique: true},
-  description: String
-});
-
-var Pickups = mongoose.model('Pickups', pickupsSchema);
-
-
-
-
-
-// Prepare data
 
 // arrays containing data for each table
 var convStartsArr = ['Do you play any instruments?', 'Do you have any siblings?', 'What is the wierdest dream you can remember?', 'What was your first job?', 'Do you play any sports?', 'What type of music do you listen to?', 'What is the best piece of advice you\'ve received?', 'What is your favorite board game?', 'If you had a million dollars but could only spend it in one store, where would you spend it?', 'What is your favorite season?'];
@@ -62,46 +32,51 @@ var jokesArr = ['Two satellites decided to get married. The wedding wasn\'t much
 var quotesArr = ['Don\'t cry because it\'s over, smile because it happened. ~Dr. Seuss', 'A room without books is like a body without a soul. ~ Marcus Tullius Cicero', 'Keep your face always toward the sunshine - and shadows will fall behind you. ~Walt Whitman', 'Not all those who wander are lost. ~ J.R.R. Tolkien', 'Whoever is happy will make others happy too. ~Anne Frank', 'The secret of getting ahead is getting started. ~Mark Twain', 'In three words I can sum up everything I\'ve learned about life: it goes on. ~ Robert Frost', 'Lots of people want to ride with you in the limo, but what you want is someone who will take the bus with you when the limo breaks down. ~Oprah Winfrey', 'Set the controls for the heart of the sun. ~Pink Floyd', 'Then as it was, then again it will be; though the course may change sometimes, rivers always reach the sea. ~Led Zeppelin'];
 var pickupsArr = ['Are you a magician? Because whenever I look at you, everyone else disappears!', 'If you were a vegetable, you\'d be a cute-cumber', 'Do you live in a corn field, cause I\'m stalking you.', 'Are you a parking ticket? Cause you\'ve got fine written all over you.', 'I seem to have lost my phone number. Can I have yours?', 'Have you been to the doctor lately? Cause I think you\'re lacking some Vitamin Me.', 'You\'re so beautiful that you made me forget my pickup line.', 'Are you a banana? Cause I find you a-peeling.', 'My love for you is like diarrhea, I just can\'t hold it in.', 'Can I borrow a kiss? I promise I\'ll give it back.'];
 
-// turn array of strings into array of objects with description property
-var createObjs = arr => {
-  var results = [];
-  arr.forEach(item => {
-    results.push({description: item});
+// create a document (row) for each conversation starter
+convStartsArr.forEach((starter, i) => {
+  let newStarter = new Convstarts({description: starter, id: i});
+  newStarter.save((err, data) => {
+    if (err) {
+      console.error(err);
+    }
   });
-  return results;
-};
+});
 
-// array containing objects for each entry
-var convStartsObjsArr = createObjs(convStartsArr);
-var jokesObjsArr = createObjs(jokesArr);
-var quotesObjsArr = createObjs(quotesArr);
-var pickupsObjsArr = createObjs(pickupsArr);
+// create a document (row) for each joke
+jokesArr.forEach((joke, i) => {
+  let newJoke = new Jokes({description: joke, id: i});
+  newJoke.save((err, data) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
 
+// create a document (row) for each quote
+quotesArr.forEach((quote, i) => {
+  let newQuote = new Quotes({description: quote, id: i});
+  newQuote.save((err, data) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
 
-
-
-
-
-// Create tables from data
-
-// insert data into appropriate table
-var addToTable = (arr, table) => {
-  db[table].insert(arr);
-};
-
-// create tables from array data
-addToTable(convStartsObjsArr, Convstarts);
-addToTable(jokesObjsArr, Jokes);
-addToTable(quotesObjsArr, Quotes);
-addToTable(pickupsObjsArr, Pickups);
-
-
+// create a document (row) for each pickup line
+pickupsArr.forEach((pickup, i) => {
+  let newPickup = new Pickups({description: pickup, id: i});
+  newPickup.save((err, data) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
 
 
 
 
 // invokes a callback on a randomly chosen item from a the Convstarts table
-var selectRandom = function(callback) {
+var selectRandomStarter = function(callback) {
   Convstarts.find({id: Math.floor(Math.random() * 10)}, function(err, items) {
     if(err) {
       callback(err, null);
@@ -111,4 +86,40 @@ var selectRandom = function(callback) {
   });
 };
 
-module.exports.selectRandom = selectRandom;
+// invokes a callback on a randomly chosen item from a the Jokes table
+var selectRandomJoke = function(callback) {
+  Jokes.find({id: Math.floor(Math.random() * 10)}, function(err, items) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, items);
+    }
+  });
+};
+
+// invokes a callback on a randomly chosen item from a the Jokes table
+var selectRandomQuote = function(callback) {
+  Quotes.find({id: Math.floor(Math.random() * 10)}, function(err, items) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, items);
+    }
+  });
+};
+
+// invokes a callback on a randomly chosen item from a the Jokes table
+var selectRandomPickup = function(callback) {
+  Pickups.find({id: Math.floor(Math.random() * 10)}, function(err, items) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, items);
+    }
+  });
+};
+
+module.exports.selectRandomStarter = selectRandomStarter;
+module.exports.selectRandomJoke = selectRandomJoke;
+module.exports.selectRandomQuote = selectRandomQuote;
+module.exports.selectRandomPickup = selectRandomPickup;
